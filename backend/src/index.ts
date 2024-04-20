4c5f8497b75fefec91d64c981560599517f798f8
jominr <jominr@qq.com>
2024-04-20 22:16:21 +1000

    automated tests for register and login

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,11 @@ import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
 import cookieParser from "cookie-parser";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(()=>{ 
+    console.log("Connected to database:", 
+    process.env.MONGODB_CONNECTION_STRING)
+  });
 
 
 const app = express();
