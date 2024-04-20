fee93fc77013cade495821c4586c35f58f51aee3
jominr <jominr@qq.com>
2024-04-20 19:45:42 +1000

    loginIn and logOut

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './Layout';
 import Register from './pages/Register';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
@@ -10,12 +11,19 @@ function App() {
         <Route path="/" element={<Layout>
           <p>Home Page</p>
         </Layout>}/>
+
         <Route path="/search" element={<Layout>
           <p>Search Page</p>
         </Layout>} />
+
         <Route path="/register" element={<Layout>
           <Register />
         </Layout>} />
+
+        <Route path="/sign-in" element={<Layout>
+          <SignIn />
+        </Layout>} />
+
         <Route path="*" element={<Navigate to="/" />}/>
       </Routes>
     </BrowserRouter>
